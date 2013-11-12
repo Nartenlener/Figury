9328fe201bdf0afbd60e7bd6d4d6a47c0b6be207
Nart <morrowind1@wp.pl>
2013-11-12 18:01:09 +0100

    Dodanie testów

---
+++ src/klasaZTestami/testy.java
@@ -0,0 +1,29 @@
+package klasaZTestami;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testy {
+
+	@Test
+	public void testA() {
+		assertEquals(7, 6);
+	}
+	
+	@Test
+	public void testB() {
+		assertEquals(3, 3);
+	}
+	
+	@Test
+	public void testC() {
+		assertEquals(3, 4);
+	}
+	
+	@Test
+	public void testD() {
+		assertEquals(3, 7);
+	}
+	
+}
